6c7414bad2aa48de9c2f6cd119782baed8748bc4
Ezer <moutinhoezer@gmail.com>
2026-09-22 11:52:34 -0300

    deploy: atualiza visual Rituá, restaura logo e adiciona crédito PIXELRY

--- src/components/Brand.jsx
+++
@@ -1,9 +0,0 @@
-import logo from '../assets/logo.png'
-
-export default function Brand() {
-  return (
-    <span className="brand">
-      <img src={logo} alt="Studio Rituá" />
-    </span>
-  )
-}
